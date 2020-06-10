7a3a165423d18353bea9113a4bc6ac20db7a1688
edbrmm <ricardo.miyoshi@gmail.com>
2020-06-10 10:13:27 -0300

    Criado tabela de eventos para a agenda

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import Evento
 
 # Register your models here.
+
+class EventoAdmin(admin.ModelAdmin): # customiza a apresentação na página admin to Django
+    list_display = ('titulo', 'data_evento', 'data_criacao') 
+    list_filter = ('usuario','data_evento',) # necessário manter uma virgula no final da lista
+
+admin.site.register(Evento, EventoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User #importa a tabela de usuário do Django
 
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank =True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'evento' # força o nome da tabela como evento, caso contrário ele coloca o nome do app antes de evento, neste caso core_evento
+
+    def __str__(self):
+        return self.titulo # para que quando o registro for acessado, o nome do título seja mostrado
