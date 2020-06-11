63c258355b1948055ebf26d7a456b54597c7d86c
edbrmm <ricardo.miyoshi@gmail.com>
2020-06-11 10:34:53 -0300

    adicionados funções de alteração e exclusão

--- core/admin.py
+++ core/admin.py
@@ -4,7 +4,7 @@ from core.models import Evento
 # Register your models here.
 
 class EventoAdmin(admin.ModelAdmin): # customiza a apresentação na página admin to Django
-    list_display = ('titulo', 'data_evento', 'data_criacao') 
+    list_display = ('id', 'titulo', 'data_evento', 'data_criacao', 'usuario') 
     list_filter = ('usuario','data_evento',) # necessário manter uma virgula no final da lista
 
 admin.site.register(Evento, EventoAdmin)
