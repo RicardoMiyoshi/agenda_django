11ffb67b21dd333ba3c18cf268347444f77d7f13
edbrmm <ricardo.miyoshi@gmail.com>
2020-06-14 17:38:44 -0300

    Final

--- agenda/urls.py
+++ agenda/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('agenda/', views.lista_eventos),
     path('agenda/lista/', views.json_lista_evento),
     path('agenda/evento/', views.evento),
+    path('agenda/evento/historico/', views.historico),
     path('agenda/evento/submit', views.submit_evento),
     path('agenda/evento/delete/<int:id_evento>/', views.delete_evento),
     path('', RedirectView.as_view(url='/agenda')),
